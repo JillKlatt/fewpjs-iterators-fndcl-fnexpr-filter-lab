1b36326fc91337906ae0311b9b8b47d2f62a4a2c
JillKlatt <klatt.jill@gmail.com>
2021-05-19 13:01:24 -0400

    commit tree c4e03b74fecfb7fad50f467301c73a5b73714391

--- index.js
+++ index.js
@@ -1,1 +1,31 @@
 // Code your solution here
+function findMatching(drivers, string) {
+    // console.log(drivers)
+    // console.log(string)
+    return drivers.filter( driver => 
+        driver.toLowerCase() === string.toLowerCase()
+    )
+}
+
+function fuzzyMatch(drivers, string) {
+    let splitString = string.split('')
+    return drivers.filter( driver => {
+        let splitDriver = driver.split('')
+        splitDriver[0].toLowerCase() === splitString[0].toLowerCase()
+        console.log(splitDriver[0])
+        console.log(splitString[0])
+
+    })
+}
+
+// function fuzzyMatch(source, testString) {
+//     return source.filter( possibleMatch =>
+//       possibleMatch.toLowerCase().indexOf(testString.toLowerCase()) === 0
+//     )
+//   }
+
+function matchName(drivers, string){
+    return drivers.filter(driver => 
+        driver.name === string 
+    )     
+}
